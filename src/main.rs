f02cb04968765891bdba4264a2ab7defa17cd04c
saintabomination <saint.abomination@protonmail.com>
2022-10-13 21:58:47 +0200

    Creating the main loop for inputting the letters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hangman CLI\n");
+
+    loop {
+        print!("Enter a letter: ");
+        io::stdout().flush().expect("Flush failed!");
+
+        let mut letter = String::new();
+        io::stdin()
+            .read_line(&mut letter)
+            .expect("Your input is not valid.");
+
+        println!("{}", letter);
+    }
 }
