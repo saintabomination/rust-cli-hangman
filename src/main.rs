a73b7a13c9b780b647bf8f13b2738309df31aa53
saintabomination <saint.abomination@protonmail.com>
2022-10-14 09:08:55 +0200

    Saving the matched characters

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use std::io;
 use std::io::Write;
 
-fn print_word(word: &String) {
+fn print_word(word: &String) -> () {
     let character_count = word.chars().count();
-    println!("{}", "_".repeat(character_count));
+    println!("{} ({})", "_".repeat(character_count), word);
+}
+
+fn check_guess(word: &String, guessed_letter: &char, shown_letters: &mut Vec<char>) -> () {
+    if !word.contains(*guessed_letter) {
+        return;
+    };
+    if !shown_letters.contains(guessed_letter) {
+        shown_letters.push(*guessed_letter);
+    }
+    println!("{:?}", shown_letters);
 }
 
 fn main() {
     println!("Hangman CLI\n");
     let my_word = String::from("abacus");
+    let mut shown_letters: Vec<char> = Vec::new();
 
     loop {
         print!("Enter a letter: ");
@@ -20,5 +31,6 @@ fn main() {
             .expect("Your input is not valid.");
 
         print_word(&my_word);
+        check_guess(&my_word, &letter.trim().chars().next().unwrap(), &mut shown_letters);
     }
 }
